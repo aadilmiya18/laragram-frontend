c4ab4c6eb5104513a502461c20577ec3ec96a37c
Aadil Miya <miyaaadil99@ambition.guru>
2025-08-29 23:47:19 +0545

    feat: integrate Tailwind CSS and update PostCSS configuration
    - Added Tailwind CSS as a dependency and configured it in postcss.config.js.
    - Updated app.scss to include Tailwind's base, components, and utilities.
    - Modified IndexPage.vue to utilize Tailwind classes for styling.
    - Updated quasar.config.js to enable PostCSS.
    - Cleaned up package.json by removing duplicate dependencies and updating versions.
    - Created a new tailwind.config.js file for Tailwind CSS configuration.

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,9 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+  <q-page class="flex items-center justify-center">
+    <span class="tw-text-lg tw-font-bold tw-text-blue-500">haha</span>
   </q-page>
 </template>
+
 
 <script setup>
 //
